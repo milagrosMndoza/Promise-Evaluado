21f88c5fc6515c943571e2ad2cfc7ab9c32a153a
milagrosMndoza <milagrosmndoza@gmail.com>
2017-11-13 16:59:53 -0500

    subiendo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, NavLink, Switch, Redirect } from 'react-router-dom';
-import {SingIn} from './SingIn';
-import {SingUp} from './SingUp';
-import {BoardTrello} from './BoardTrello';
+// import {SingIn} from './SingIn';
+// import {SingUp} from './SingUp';
+// import {BoardTrello} from './BoardTrello';
 import { connect } from 'redux-zero/react';
 
-const App =({board, user})=> {
+const App =({})=> {
 return (
  <BrowserRouter>
       <Switch>
-        <Route exact path="/" render={() => <SingIn />} />
-        <Route exact path="/singIn" render={() => <SingIn />} /> 
-        <Route exact path="/singUp" render={() => <SingUp />} />
-        <Route exact path="/BoardTrello" render={() => <BoardTrello />} />
+        <Route exact path="/" render={() => <App />} />
+        <Route exact path="/" render={() => <App />} /> 
+        <Route exact path="/" render={() => <App />} />
+        <Route exact path="/" render={() => <App />} />
         <Route render={() => <Redirect to={"/"} />}/>
       </Switch>
 </BrowserRouter> 
@@ -22,8 +22,8 @@ return (
 )
 }
 /* <SingUp/> */
-const mapToProps = ({ board, user }) => ({
-    board, user 
+const mapToProps = ({  }) => ({
+     
  });
  export default connect(mapToProps)(App);
 
