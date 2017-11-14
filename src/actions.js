71a58c30ec0dbcf1b73774529e7fb197ffe19751
milagrosMndoza <milagrosmndoza@gmail.com>
2017-11-14 08:37:04 -0500

    avanzando

--- src/actions.js
+++ src/actions.js
@@ -1,2 +1,32 @@
 import store from './store.js';
 
+export async function search (text) {
+    const url = text;
+    fetch(url)
+        .then(res => res.json())
+        .then(res => {
+            let items = [...store.getState().items];
+            let newPlanet = items.concat({
+                name : res.pl_name,
+                dens : res.pl_dens,
+                telescope : res.pl_telescope,
+                img : res.img,
+                year : res.pl_disc
+            });
+            store.setState({
+                items : newPlanet
+            });
+        })
+}
+
+export async function getPlanets() {
+    const url = "data/earth-like-results.json";
+    fetch(url)
+    .then(res => res.json())
+    .then(res => {
+        res.results.map(planet => {
+            return (search(planet));
+        })
+    })
+}
+
--- src/store.js
+++ src/store.js
@@ -1,22 +1,9 @@
 import createStore from 'redux-zero';
 
 const initialState = {
-    board: [], 
-    user: [
-        {
-            firstName: "Milagros",
-            lastName: "Mendoza",
-            email: "milagrosmndoza@gmail.com", 
-            password: "000000",
-            boards: [
-                {
-                    id: 0,
-                    title: "Aprender Programación"
-                }
-            ]
-        }
-    ]
+    items : []
 }
 
 const store = createStore(initialState);
-export default store;
+export default store; 
+
